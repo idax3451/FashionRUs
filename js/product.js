7f4e69aba528260cb3abcdae34ed027c7d0d06f6
Ida <idax3451@stud.kea.dk>
2025-09-29 16:33:33 +0200

    adding changes

--- js/product.js
+++ js/product.js
@@ -17,19 +17,15 @@ function showProduct(product) {
         <!-- Midt: Info -->
         <div class="product_info">
           <h2>Product Information</h2>
-          <p class="modelName"><strong>Model name</strong><br />Sahara Team India Fanwear Round Neck Jersey ${product.productdisplayname}</p>
-          <p class="modelColor"><strong>Color</strong><br />Blue ${product.colour1}</p>
-          <p class="inventoryNumber"><strong>Inventory number</strong><br />1163 ${product.price}</p>
-
-          <h3>Nike</h3>
-          <p>Nike, creating experiences for today’s athlete</p>
-          <p class="articleType"><span class="bold">Type:</span> ${product.articletype}</p>
+          <p class="modelName"><strong>Brand name</strong><br/> ${product.brandname}</p>
+          <p class="modelName"><strong>Model name</strong><br/> ${product.productdisplayname}</p>
+          <p class="modelColor"><strong>Color</strong><br/>Blue ${product.colour1}</p>
+          <p class="articleType"><span class="bold"> <strong>Type</strong><br/></span> ${product.articletype}</p>
         </div>
 
         <!-- Højre: Køb-boks -->
         <aside class="product_buybox">
-          <h2>Sahara Team India Fanwear Round Neck Jersey</h2>
-          <p class="productCategory">Nike | Tshirts ${product.category}</p>
+          <h2>${product.productdisplayname}</h2>
 
           <label for="size">Choose a size</label>
           <select id="size" name="size">

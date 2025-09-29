717630e46b39f0abcbc5461185d245779f3d52a2
Ida <idax3451@stud.kea.dk>
2025-09-29 14:57:49 +0200

    add if-else

--- js/productlist.js
+++ js/productlist.js
@@ -1,6 +1,11 @@
-const product_list_container = document.querySelector("main");
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+console.log(category);
 
-fetch(`https://kea-alt-del.dk/t7/api/products`)
+const product_list_container = document.querySelector("main");
+const header = (document.querySelector("h2").textContent = category);
+
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&category=${category}`)
   .then((response) => response.json())
   .then((data) => showProducts(data));
 
@@ -9,19 +14,18 @@ function showProducts(products) {
   products.forEach((element) => {
     console.log(element);
     product_list_container.innerHTML += ` <!-- Produktkort 4 (udsolgt + rabat) -->
-         <a class="product_card soldout">
+         <article class="product_card">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="product image" />
           <span class="soldout_label">Sold Out</span>
           <h3>${element.productdisplayname}</h3>
           <p class="category">${element.articletype}</p>
           <p class="price">
-            <span class="prev">Prev. DKK ${element.price}</span><br />
-            <span class="now">Now DKK ${element.price}</span>
+            <span class= ${element.discount && "prev"}>DKK ${element.price}</span><br />
+            
+            ${element.discount ? `<span class="now">Now DKK ${element.price}</span><span class="discount">-34%</span>` : ""}
           </p>
-          <span class="discount">-34%</span>
-          <p class="readmore">Read More</p>
-          <a href="product.html?id=${element.id}">Read More</a>
-        </a>`;
+          <a class="readmore" href="product.html?id=${element.id}">Read More</a>
+        </article>`;
   });
 }
 
